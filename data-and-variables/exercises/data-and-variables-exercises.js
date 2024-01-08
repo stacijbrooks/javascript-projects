aecfb26d603564253d90a53e2ec1bb8f14f74870
stacijbrooks <stacibrooksfitness@gmail.com>
2024-01-07 19:46:37 -0600

    Commit

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -12,9 +12,14 @@ console.log(typeof distanceToMoonKm);
 console.log(typeof milesPerKm);
 
 // Calculate a space mission below
-
+let milesToMars = distanceToMarsKm * milesPerKm;
+let hoursToMars = milesToMars / shuttleSpeedMph;
+let daysToMars = hoursToMars / 24;
 // Print the results of the space mission calculations below
-
+console.log(shuttleName + " will take " + daysToMars + "days to reach Mars.");
 // Calculate a trip to the moon below
-
+let milesToMoon = distanceToMoonKm * milesPerKm;
+let hoursToMoon = milesToMoon / shuttleSpeedMph
+let daysToMoon = hoursToMoon / 24;
 // Print the results of the trip to the moon below
+console.log(shuttleName + " will take " + daysToMoon + " days to reach the Moon. ");
