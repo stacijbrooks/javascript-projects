45f36791feda24ea387d6c8a6b1c04bdf02b7153
stacijbrooks <stacibrooksfitness@gmail.com>
2024-01-11 17:43:40 -0600

    1st part of Shuttle Studio

--- booleans-and-conditionals/studio/data-variables-conditionals.js
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -1,9 +1,24 @@
 // Initialize Variables below
-
+let date = "Monday 2019-03-18";
+let time = "10:05:34";
+let astronautCount = 7;
+let astronautStatus = "ready";
+let averageAstronautMassKg = 80.7;
+let crewMassKg = astronautCount * averageAstronautMassKg;
+let fuelMassKg = 760000;
+let shuttleMassKg = 74842.31;
+let totalMassKg = crewMassKg + fuelMassKg + shuttleMassKg;
+let maximumMassLimit = 850000;
+let fuelTempCelsius = -225;
+let minimumFuelTemp = -300;
+let maximumFuelTemp = -150;
+let fuelLevel = "100%";
+let weatherStatus = "clear";
+let preparedForLiftOff = true;
 // add logic below to verify total number of astronauts for shuttle launch does not exceed 7
-
+if (astronautCount <= 7) {
 // add logic below to verify all astronauts are ready
-
+} else if (astronautStatus = "ready"); {
 // add logic below to verify the total mass does not exceed the maximum limit of 850000
 
 // add logic below to verify the fuel temperature is within the appropriate range of -150 and -300
