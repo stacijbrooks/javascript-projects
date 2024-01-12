acc49447c5e68e2862c8441234f71831ead0854c
stacijbrooks <stacibrooksfitness@gmail.com>
2024-01-11 20:24:59 -0600

    1st Shuttle Update

--- booleans-and-conditionals/studio/data-variables-conditionals.js
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -17,14 +17,20 @@ let weatherStatus = "clear";
 let preparedForLiftOff = true;
 // add logic below to verify total number of astronauts for shuttle launch does not exceed 7
 if (astronautCount <= 7) {
+    console.log("Keep going!");
 // add logic below to verify all astronauts are ready
-} else if (astronautStatus = "ready"); {
+} else if (astronautStatus === "ready") {
+    console.log("Ready!"); 
 // add logic below to verify the total mass does not exceed the maximum limit of 850000
-
+} else if (totalMassKg < maximumMassLimit) {
+    console.log("engines are preparing to start");
 // add logic below to verify the fuel temperature is within the appropriate range of -150 and -300
-
+} else if (fuelTempCelsius >= -300 && fuelTempCelsius <= -150) {
+    console.log("Fuel temp is between -300 and -150");
 // add logic below to verify the fuel level is at 100%
-
+} else if (fuelLevel === 100) {
+    console.log("Fuel level is at 100%");
 // add logic below to verify the weather status is clear
-
-// Verify shuttle launch can proceed based on above conditions
+} else if (weatherStatus === "clear") {
+    console.log("Weather is clear!");
+// Verify shuttle launch can proceed based on above conditions//
