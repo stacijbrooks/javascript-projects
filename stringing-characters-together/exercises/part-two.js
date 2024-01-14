b815eb5e7a607885dc1d0d850901349280b153cc
stacijbrooks <stacibrooksfitness@gmail.com>
2024-01-14 14:55:28 -0600

    DNA Part 2 Section 1

--- stringing-characters-together/exercises/part-two.js
+++ stringing-characters-together/exercises/part-two.js
@@ -3,18 +3,18 @@
 let dna = " TCG-TAC-gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT    ";
 
 // First, print out the dna strand in it's current state.
-
+console.log(" TCG-TAC-gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT    ")
 //1) Use the .trim() method to remove the leading and trailing whitespace, then print the result.
-
-console.log(/* Your code here. */);
+let newString = dna.trim();
+console.log(newString);
 
 //2) Change all of the letters in the dna string to UPPERCASE, then print the result.
-
-console.log();
+dna = dna.trim().toUpperCase();
+console.log(dna);
 
 //3) Note that after applying the methods above, the original, flawed string is still stored in dna. To fix this, we need to reassign the changes to back to dna.
 //Apply these fixes to your code so that console.log(dna) prints the DNA strand in UPPERCASE with no whitespace.
-
+dna = "TCG-TAC-gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT".toUpperCase();
 console.log(dna);
 
 //Part Two Section Two
