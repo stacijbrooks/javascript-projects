ea9fb07e79bd1a59ebbba373341db9b07acf873b
stacijbrooks <stacibrooksfitness@gmail.com>
2024-01-18 17:33:00 -0600

    Part 3

--- arrays/exercises/part-three-arrays.js
+++ arrays/exercises/part-three-arrays.js
@@ -14,3 +14,5 @@ console.log(cargoHold);
 
 
 //3) Replace the elements at indexes 2 - 4 with the items ‘cat’, ‘fob’, and ‘string cheese’.
+cargoHold.splice(2,3, 'cat', 'fob', 'string cheese');
+console.log(cargoHold);
