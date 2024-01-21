a0997ef416662f49b596574812a849c651628872
stacijbrooks <stacibrooksfitness@gmail.com>
2024-01-20 19:13:01 -0600

    Update Loop Practice

--- loops/chapter-examples/for-Loop-Practice-With-Strings.js
+++ loops/chapter-examples/for-Loop-Practice-With-Strings.js
@@ -1,4 +1,6 @@
 // Create a string variable containing your name.
-
-
+let name = "StaciBrooks"
 // Write a for loop that prints each character in your name on a different line.
+for(let i=0; i < name.length; i++) {
+    console.log(name[i]);
+}
