e53b19acbf7c09a9ea70a00f1afe1fcb927e7175
stacijbrooks <stacibrooksfitness@gmail.com>
2024-01-21 14:22:43 -0600

    While Loop Examples

--- loops/chapter-examples/while-Loop-Example.js
+++ loops/chapter-examples/while-Loop-Example.js
@@ -4,3 +4,12 @@ while (i < 51) {
   console.log(i);
   i++;
 }
+const input = require('readline-sync');
+
+let num = input.question('Please enter a positive number:');
+num = Number(num);
+
+while (num <= 0) {
+   num = input.question('Invalid input. Please enter a positive number:');
+   num = Number(num);
+}
