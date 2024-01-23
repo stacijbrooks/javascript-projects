f5c6db3080f239240f8b2a2ec432c7d8e03ae317
stacijbrooks <stacibrooksfitness@gmail.com>
2024-01-22 22:35:59 -0600

    While loops attempt 2

--- loops/exercises/while-Loop-Exercises.js
+++ loops/exercises/while-Loop-Exercises.js
@@ -9,15 +9,22 @@ let fuelLevel = 0, numAstronauts = 0, altitude = 0;
 
 /*Exercise #4: Construct while loops to do the following:
   a. Query the user for the starting fuel level. Validate that the user enters a positive, integer value greater than 5000 but less than 30000. */
-  while (fuelLevel <= 5000 || fuelLevel > 30000 || isNaN(fuelLevel)) {
+  while (fuelLevel <= 5000 || fuelLevel >= 30000 || isNaN(fuelLevel)) {
     fuelLevel = input.question("Enter the starting fuel level: ");
+    fuelLevel = Number(fuelLevel);
+    let alert = ("Invalid fuel level. Please enter a positive value greater than 5,00 but less than 30000");
  }
+ console.log(alert);
 //It just repeats the starting fuel level...still working on
 
 
 
 //b. Use a second loop to query the user for the number of astronauts (up to a maximum of 7). Validate the entry.
-  
+  numAstronauts = input.question("Enter the number of astronauts between 1 and 7");
+  numAstronauts = Number(numAstronauts);
+  while (isNaN(numAstronauts) || numAstronauts < 1 || numAstronauts > 7) {
+    console.log ("Invalid number of astronauts. Please enter an integer between 1 and 7.");
+  }
   
   
   
