a6bc8e21ac0c6adc60533601894b57c7c48e6db8
stacijbrooks <stacibrooksfitness@gmail.com>
2024-01-24 17:08:30 -0600

    Functions progress

--- functions/javascript-projects-functions.js
+++ functions/javascript-projects-functions.js
@@ -5,5 +5,23 @@ for (let i=0; i < size; i++) {
 }
 return line;
 }
-console.log(makeLine(5));
-//not sure why I put a 5 here though? Everything else I think I understand.
+console.log(makeLine(10));
+
+
+//MAKE SQUARE SIZE
+function makeSquare(size) {
+    let square = '';
+for (i=0; i < size; i++) {
+    square += (makeSquare(size) + '\n');
+}
+}
+
+ 
+//MAKE RECTANGLE HEIGHT AND WIDTH
+function makeRectangle(width, height) {
+    let rectangle = '';
+for (let i=0; i < height; i++) {
+    rectangle += (makeLine(width) + '\n');
+}
+return rectangle.slice(0,-1);
+}
