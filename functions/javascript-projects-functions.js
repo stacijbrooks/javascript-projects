535d00ae85b32da088d2dc1781423670e698b038
stacijbrooks <stacibrooksfitness@gmail.com>
2024-01-23 22:45:29 -0600

    Javascript functions new file

---
+++ functions/javascript-projects-functions.js
@@ -0,0 +1,9 @@
+function makeLine(size) {
+    let line = '';
+for (let i=0; i < size; i++) {
+    line += '#';
+}
+return line;
+}
+console.log(makeLine(5));
+//not sure why I put a 5 here though? Everything else I think I understand.
