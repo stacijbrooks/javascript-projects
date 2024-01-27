6020fd2528f5de1716b38532ada4fc2d98c1eef5
stacijbrooks <stacibrooksfitness@gmail.com>
2024-01-27 12:42:45 -0600

    Get Valid Input Exercise DONE

--- more-on-functions/chapter-examples/receieve-function-arguments/getValidInput.js
+++ more-on-functions/chapter-examples/receieve-function-arguments/getValidInput.js
@@ -14,8 +14,19 @@ function getValidInput(prompt, isValid) {
 
 // TODO 1: write a validator 
 // that ensures input starts with "a"
-
+let startsWithA = function(input)  {
+  return input.toLowerCase().startsWithA("a");
+};
 // TODO 2: write a validator 
 // that ensures input is a vowel
-
+let isVowel = function(input) {
+  return ['a', 'e', 'i', 'o','u'].indexOf(input.toLowerCase());
+}
+let isValidPasword = function(password){
+  if (password.length < 8) {
+    return false;
+  }
+  else return true;
+}
 // Be sure to test your code!
+console.log(getValidInput('Create a password:', isValidPasword));
