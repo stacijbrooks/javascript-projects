962b4ac5b144aa6f6f42dfa85fd7eadfd2e1ceea
stacijbrooks <stacibrooksfitness@gmail.com>
2024-01-31 20:28:20 -0600

    Kindness Selection can't print 3 each

--- objects-and-math/chapter-examples/KindnessSelection.js
+++ objects-and-math/chapter-examples/KindnessSelection.js
@@ -1,17 +1,29 @@
-function randomSelection(arr){
-   let index = Math.floor(Math.random()*arr.length);
-   return arr[index];
- }
+// function randomSelection(arr){
+//    let index = Math.floor(Math.random()*arr.length);
+//    return arr[index];
+//  }
  
- let happiness = ['Hope', 'Joy', 'Peace', 'Love', 'Kindness', 'Puppies', 'Kittens', 'Tortoise'];
+//  let happiness = ['Hope', 'Joy', 'Peace', 'Love', 'Kindness', 'Puppies', 'Kittens', 'Tortoise'];
  
- let words = ['Hello', 'World', 'Python', 'JavaScript', 'Rutabaga'];
+//  let words = ['Hello', 'World', 'Python', 'JavaScript', 'Rutabaga'];
  
- for (i=0; i < 8; i++){
-   console.log(randomSelection(happiness));
- }
- 
+//  for (i=0; i < 8; i++){
+//    console.log(randomSelection(happiness));
+//  }
+//  console.log(randomSelection(words));
+
  //Experiment with the code above. Try to:
  //a) Print 3 random selections from each array.
  //b) Have the code randomly pick one array, and then print 2 random items from it.
  //c) Create a new array, then fill it with one random item from words and happiness. Print the new array.
+function randomSelection(arr) {
+  let index = Math.floor(Math.random() * arr.length);
+  return arr[index];
+}
+
+let happiness = ['Hope', 'Joy', 'Peace', 'Love', 'Kindness', 'Puppies', 'Kittens', 'Tortoise'];
+let words = ['Hello', 'World', 'Python', 'JavaScript', 'Rutabaga'];
+
+let selectedArray = [randomSelection(happiness), randomSelection(words)];
+console.log([randomSelection(happiness, 3)]);
+console.log([randomSelection(words, 3)]);
