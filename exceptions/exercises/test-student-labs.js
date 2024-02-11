d1ebaea2438bcde01d3f1aaa774735df6e487ca0
stacijbrooks <stacibrooksfitness@gmail.com>
2024-02-10 21:07:49 -0600

    test-student-labs

--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -1,7 +1,45 @@
+// function gradeLabs(labs) {
+//   for (let i=0; i < labs.length; i++);
+//     let lab = labs[i];
+//     {
+//       try {
+//         let result = lab.runLab(3);
+//   } catch (error) {
+//     console.log(`Error evaluating lab for ${lab.student}: ${error.message}`);
+//     result = "Error thrown";
+//   }
+//     console.log(`${lab.student} code worked: ${result === 27}`);
+//   };
+
+// let studentLabs = [
+//   {
+//     student: 'Carly',
+//     runLab: function (num) {
+//         return Math.pow(num, num);
+//     }
+//   },
+//   {
+//     student: 'Erica',
+//     runLab: function (num) {
+//         return num * num;
+//     }
+//   }
+// ];
+
+// gradeLabs(studentLabs);
+// };
+// console.log(result);
+
 function gradeLabs(labs) {
-  for (let i=0; i < labs.length; i++) {
+  for (let i = 0; i < labs.length; i++) {
     let lab = labs[i];
-    let result = lab.runLab(3);
+    let result;
+    try {
+      result = lab.runLab(3);
+    } catch (error) {
+      console.log(`Error evaluating lab for ${lab.student}: ${error.message}`);
+      result = "Error thrown";
+    }
     console.log(`${lab.student} code worked: ${result === 27}`);
   }
 }
