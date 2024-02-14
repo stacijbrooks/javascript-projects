c9fc40ebf97139104a576a444d2712a87028ae12
stacijbrooks <stacibrooksfitness@gmail.com>
2024-02-13 20:59:07 -0600

    Class Examples02

--- classes/chapter-examples/ClassExamples02.js
+++ classes/chapter-examples/ClassExamples02.js
@@ -3,7 +3,7 @@
 // Next, set default values for 1 or more of the parameters in constructor.
 
 class Astronaut {
-   constructor(name, age, mass){
+   constructor(name = "Max", age = 67, mass = 54){
      this.name = name;
      this.age = age;
      this.mass = mass;
