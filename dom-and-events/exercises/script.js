5101674303393bb92add1c4e6589d137d450a1c3
stacijbrooks <stacibrooksfitness@gmail.com>
2024-02-25 18:15:38 -0600

    Update script.js

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,24 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    button.addEventListener('click', event => {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+     }); 
+     missionAbort.addEventListener('mouseover', event => {
+        missionAbort.style.backgroundColor = 'red';
+     });
+   
+    missionAbort.addEventListener("mouseout", function( event ) {
+        event.target.style.backgroundColor = "";
+    });
+    missionAbort.addEventListener('click', event => {
+        //prompt the user with a confirmation window
+        let abortConfirmed = window.confirm("Are you sure you want to abort the mission?");
+        //if user confirms...update text
+        if (abortConfirmed) {
+            paragraph.innerHTML = 'Mission aborted! Space shuttle returning home.';
+        }
+    });
 }
 
 window.addEventListener("load", init);
