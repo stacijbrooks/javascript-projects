c554dbfad160fd77fa1856b7971cf06390600bb8
stacijbrooks <stacibrooksfitness@gmail.com>
2024-03-10 19:33:39 -0500

    React changes

---
+++ react-part-one/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Hello from "./components/Hello"
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+        <Hello />
+      </div>
+      
+    </>
+  )
+}
+
+export default App
